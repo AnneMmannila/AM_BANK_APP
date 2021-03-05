63b390a80e282a50e989f88de7b1293086c69064
AnneMmannila <70891200+AnneMmannila@users.noreply.github.com>
2021-03-05 16:37:43 +0200

    first commmit

---
+++ src/main/java/com/example/AM_BANK_APP/AmBankAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.AM_BANK_APP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmBankAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmBankAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/AM_BANK_APP/AmBankAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AM_BANK_APP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmBankAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
