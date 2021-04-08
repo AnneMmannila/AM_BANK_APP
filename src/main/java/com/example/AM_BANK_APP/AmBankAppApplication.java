d892724dd5aa87d31ebd8e9b894ec6623cfd24f5
AnneMmannila <70891200+AnneMmannila@users.noreply.github.com>
2021-04-08 14:50:11 +0300

    postgre

--- src/main/java/com/example/AM_BANK_APP/AmBankAppApplication.java
+++ src/main/java/com/example/AM_BANK_APP/AmBankAppApplication.java
@@ -34,7 +34,7 @@ public class AmBankAppApplication {
 			//https://www.bcryptcalculator.com/
 			User user1 = new User("Anna A", "$2a$10$th.y1rlTTQ.iLaNqIs9ANuHjUY1N09IV9frYAtfiOPEcueSL2/Fci","anna@gmail.com", "Anna A");
 			User user2 = new User("Bertta B", "$2a$10$/Oyf5qxpxU8q0Tsb8cwCd.gIAH9bybY5OVSgxxKvBND3n3DpXWlI.","bertta@gmail.com", "Bertta B");
-			
+			urepository.deleteAll();
 			urepository.save(user1);
 			urepository.save(user2);
 			
